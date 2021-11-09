7bd01ada67d7b247d352e20de8bfd6b1dcba11a4
mofic <lwn314@163.com>
2021-11-09 10:39:02 +0800

    Update build.sh

--- scripts/ffmpeg/build.sh
+++ scripts/ffmpeg/build.sh
@@ -42,7 +42,7 @@ DEP_LD_FLAGS="-L${BUILD_DIR_EXTERNAL}/${ANDROID_ABI}/lib $FFMPEG_EXTRA_LD_FLAGS"
   --extra-cflags="-O3 -fPIC $DEP_CFLAGS" \
   --extra-ldflags="$DEP_LD_FLAGS" \
   --enable-shared \
-  --disable-static \
+  --enable-static \
   --pkg-config=${PKG_CONFIG_EXECUTABLE} \
   ${EXTRA_BUILD_CONFIGURATION_FLAGS} \
   $ADDITIONAL_COMPONENTS || exit 1
